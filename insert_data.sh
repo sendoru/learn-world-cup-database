32a81cfd70d0fbfc83c5db08053c54064f391bc0
Min Cheol Song <sendol39@gmail.com>
2024-07-10 15:54:50 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,45 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+# init table
+($PSQL "TRUNCATE TABLE games, teams")
+
+# adding teams
+cat games.csv | while IFS=',' read year round winner opponent winner_goals opponent_goals
+do
+  # ignore first line
+  if [[ $year = 'year' ]]
+  then
+    continue
+  fi
+
+  # add winner team in the teams table only if the team doesn't exist
+  res=$($PSQL "SELECT name FROM teams WHERE name='$winner'")
+  if [[ -z $res ]]
+  then
+    ($PSQL "INSERT INTO teams(name) values('$winner')")
+  fi
+
+  # add opponent team in the teams table only if the team doesn't exist
+  res=$($PSQL "SELECT name FROM teams WHERE name='$opponent'")
+  if [[ -z $res ]]
+  then
+    ($PSQL "INSERT INTO teams(name) values('$opponent')")
+  fi
+done
+
+# adding games
+cat games.csv | while IFS=',' read year round winner opponent winner_goals opponent_goals
+do
+  # ignore first line
+  if [[ $year = 'year' ]]
+  then
+    continue
+  fi
+
+  winner_id=$($PSQL "SELECT team_id FROM teams WHERE name='$winner'")
+  opponent_id=$($PSQL "SELECT team_id FROM teams WHERE name='$opponent'")
+  ($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values($year, '$round', $winner_id, $opponent_id, $winner_goals, $opponent_goals)")
+
+done
